d25e6062d34f66c7547b92f9ae10b201679cdb07
NaSinSa <andy.k.oh@hotmail.com>
2020-02-12 14:38:54 -0500

    moving snake

--- client.js
+++ client.js
@@ -7,10 +7,36 @@ const connect = function() {
     port: 50541
   });
   conn.setEncoding('utf8'); 
+
   conn.on('connect', () => {
     console.log('Enter to Win')
     conn.write('Name: WIN')
   });
+
+  // conn.on('connect', () => {
+  //   setTimeout(() => {
+  //     conn.write("Move: down")
+  //   }, 100);
+  //   setTimeout(() => {
+  //     conn.write("Move: down")
+  //   }, 300);
+  //   setTimeout(() => {
+  //     conn.write("Move: down")
+  //   }, 500);
+  //   setTimeout(() => {
+  //     conn.write("Move: down")
+  //   }, 700);
+  // });
+
+  for (let i = 0; i < 10; i++) {
+    conn.on('connect' , () => {
+      setTimeout(() => {
+        conn.write("Move: left")
+      }, i)
+    })
+  }
+
+  
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
